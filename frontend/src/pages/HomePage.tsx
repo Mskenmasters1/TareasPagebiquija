96acc24b9a216d6619776d47c2ebdcf2c2d11021
Quico Saval <quicosv@gmail.com>
2023-05-24 17:03:43 +0200

    Cambia el título de la página home

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import { Header } from "../components/Header";
+import { aplicacion } from "./MainApp";
 
 export const HomePage = () => {
-
+useEffect(() => {
+  document.title = 'Inicio - ' + aplicacion;
+}, []);
   return (
     <>
       <Header />
