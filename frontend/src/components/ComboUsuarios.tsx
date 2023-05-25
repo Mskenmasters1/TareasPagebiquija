8cadc11548b53aa26bba6e67f9f168907b33ab0a
piscinagua <correo_javi@hotmail.com>
2023-05-25 17:58:04 +0200

    Arregla el label del select del combo usuarios.Modifica los routes de usuarios y tareas.

--- frontend/src/components/ComboUsuarios.tsx
+++ frontend/src/components/ComboUsuarios.tsx
@@ -1,46 +1,52 @@
-import { ChangeEvent, useEffect, useState } from 'react';
-import { useFetchGet } from '../hooks/useFetchGet';
-import { IComboUsuarios, IComboUsuariosResponse } from '../interfaces/comboUsuarios.interface';
+import { ChangeEvent, useEffect, useState } from "react";
+import { useFetchGet } from "../hooks/useFetchGet";
+import {
+	IComboUsuarios,
+	IComboUsuariosResponse,
+} from "../interfaces/comboUsuarios.interface";
 
 interface IComboUsuariosProps {
-  setSelected: ({ target }: React.ChangeEvent<HTMLSelectElement>) => void;
-  activa?: string;
+	setSelected: ({ target }: React.ChangeEvent<HTMLSelectElement>) => void;
+	activa?: string;
 }
 
 export const ComboUsuarios = ({ setSelected, activa }: IComboUsuariosProps) => {
-  const [usuarios, setUsuarios] = useState<IComboUsuarios[]>([]);
-  const { loading, data, status, errorFetch } = useFetchGet<IComboUsuariosResponse>(
-    'http://localhost:3000/api/usuarios',
-    true
-  );
+	const [usuarios, setUsuarios] = useState<IComboUsuarios[]>([]);
+	const { loading, data, status, errorFetch } =
+		useFetchGet<IComboUsuariosResponse>(
+			"http://localhost:3000/api/usuarios",
+			true
+		);
 
-  useEffect(() => {
-    if (status === 200) {
-      setUsuarios([{ nombre: 'Asignar a', _id: '' }, ...data.usuarios]);
-    }
-  }, [status]);
-  const selectedUsuario = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelected(e);
-  };
+	useEffect(() => {
+		if (status === 200) {
+			setUsuarios([{ nombre: "Asignar a", _id: "" }, ...data.usuarios]);
+		}
+	}, [status]);
+	const selectedUsuario = (e: ChangeEvent<HTMLSelectElement>) => {
+		setSelected(e);
+	};
 
-  return (
-    <>
-      {usuarios && status === 200 && (
-        <select
-          className="form-select"
-          label-for='Asignar a'
-          required
-          id="usuario"
-          onChange={selectedUsuario}
-          value={activa}
-        >
-          {usuarios.map((x, i) => (
-            <option key={x._id} value={x._id}>
-              {x.nombre}
-            </option>
-          ))}
-        </select>
-      )}
-    </>
-  );
+	return (
+		<>
+			{usuarios && status === 200 && (
+				<>
+					<label htmlFor="usuarios">Asignar a:</label>
+					<select
+						className="form-select"
+						required
+						id="usuario"
+						onChange={selectedUsuario}
+						value={activa}
+					>
+						{usuarios.map((x, i) => (
+							<option key={x._id} value={x._id}>
+								{x.nombre}
+							</option>
+						))}
+					</select>
+				</>
+			)}
+		</>
+	);
 };
