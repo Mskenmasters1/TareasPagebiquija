68da2eff393d0ed1c2095527188103a37bfd1032
Quico Saval <quicosv@gmail.com>
2023-05-25 16:27:38 +0200

    Arregla la ruta de crear tareas

--- frontend/src/pages/tareas/TareasRouter.tsx
+++ frontend/src/pages/tareas/TareasRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Header } from '../../components/Header';
 import { MisTareasPage } from './misTareas/MisTareasPage';
 import { Footer } from '../../components/Footer';
+import { EditorDeTareasPage } from './misTareas/EditorDeTareasPage';
 
 export const TareasRoutes = () => {
   return (
@@ -11,7 +12,7 @@ export const TareasRoutes = () => {
       <div className="container">
         <Routes>
           <Route path="mistareas" element={<MisTareasPage />} />
-          <Route path="tareas/crear" element={<CrearTareasPage />} />
+          <Route path="tareas/crear" element={<EditorDeTareasPage />} />
 
           <Route path="/*" element={<Navigate to="tareas" />} />
         </Routes>
